6fd4096636c74c56d60348ed21b7ac616ce36030
Tugrulfg <tugrultugrul6@gmail.com>
2024-05-26 09:54:46 +0300

    Try running Ultralytic model

---
+++ Ultralytics/build/CMakeFiles/Track.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tugrul/Desktop/ShowSense/Github/Ultralytics/Inference.cpp" "CMakeFiles/Track.dir/Inference.cpp.o" "gcc" "CMakeFiles/Track.dir/Inference.cpp.o.d"
+  "/home/tugrul/Desktop/ShowSense/Github/Ultralytics/track.cpp" "CMakeFiles/Track.dir/track.cpp.o" "gcc" "CMakeFiles/Track.dir/track.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
