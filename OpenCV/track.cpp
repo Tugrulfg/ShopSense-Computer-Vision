2ba1c752bf225fe099e946e52606741f82ae4162
Tugrulfg <tugrultugrul6@gmail.com>
2024-05-29 21:01:53 +0300

    Model Added

--- OpenCV/track.cpp
+++ OpenCV/track.cpp
@@ -35,48 +35,25 @@ int main(){
     std::cout << "OpenCV version: " << CV_VERSION << std::endl;
     std::vector<std::pair<int, int>> results;
     MyTracker tracker;
-    cv::dnn::Net net = cv::dnn::readNetFromONNX("../Models/YOLOTiny_LargeNOOBJ/yolo_tiny_op11.onnx");
+    cv::dnn::Net net = cv::dnn::readNetFromONNX("../Models/yolo_tiny_op10.onnx");
     std::vector<std::pair<cv::Rect2f, int>> detections;
     int sz[3]= {7, 7, 20}; 
-
-
     cv::Mat frame, input, output;
 
-    cv::VideoCapture cap("../TestVideo/20240528_164634.mp4");
-    // cv::VideoCapture cap(0);
-    std::vector<std::string> test_images = getFiles("../TestImages/");
-    std::cout << test_images.size() << std::endl;
-    //for(const std::string& path: test_images){
+    cv::VideoCapture cap(0);
     while(1){
         cv::TickMeter tm;
         tm.start();
-        cap >> frame;
 
-        if (frame.empty()) {
-            std::cerr << "Error: Unable to read a frame from the video." << std::endl;
-            break;
-        }
-        // frame = cv::imread(path);
+        cap >> frame;
         cv::resize(frame, frame, cv::Size(IMAGE_SIZE, IMAGE_SIZE));
         cv::dnn::blobFromImage(frame, input, 1.0/255.0, cv::Size(224, 224), cv::Scalar(), true, false);
+        
         net.setInput(input);
-        //std::vector<std::string> outputNames = net.getUnconnectedOutLayersNames();
-
-        // Create a vector to store the outputs
-        // std::vector<cv::Mat> outputs;
-
-        // Run the forward pass and retrieve the outputs
         output = net.forward();
         cv::Mat newmat(3, sz, output.type(), output.ptr<float>(0));
         
-        getBoxes(newmat, 7, detections);
-        //Process the outputs
-        // for (size_t i = 0; i < outputs.size(); ++i) {
-        //     std::cout << outputs[i].size << std::endl;
-        //     cv::Mat newmat(3, sz[i], outputs[i].type(), outputs[i].ptr<float>(0));
-        //     getBoxes(newmat, GRIDS[i], detections);
-        // }
-        
+        getBoxes(newmat, 7, detections);        
 
         results = tracker.update(detections);
         for(const std::pair<int, int>& result : results){
@@ -85,24 +62,13 @@ int main(){
             else
                 std::cout << "TOP: " << CLASSES[result.second] << std::endl;
         }
-        tracker.draw(frame);
+        //tracker.draw(frame);
 
-        // for(const std::pair<cv::Rect2f, int>& detection: detections){
-        //     cv::putText(frame, CLASSES[detection.second], detection.first.tl(), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 0), 2);
-        //     cv::rectangle(frame, detection.first, cv::Scalar(0, 255, 255), 2);
-        // }
-        // double fps = cv::getTickFrequency() / (end - start);
-        // std::cout << fps << std::endl;
-        // cv::putText(frame, std::to_string(fps), cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(0, 255, 0), 2);
         tm.stop();
         double inferenceTime = tm.getTimeMilli(); // In milliseconds
         double fps = 1000.0 / inferenceTime;
-        cv::putText(frame, std::to_string(fps), cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(0, 255, 0), 2);
-        cv::imshow("frame", frame);
-        if(cv::waitKey(1) == 'q')
-            break;
+        std::cout << "FPS: " << fps << std::endl;
 
-        
         detections.clear();
         results.clear();
     }
